422cc2f7ee4c59d71a738d32fb04f84dda9d1ab3
Sriharsha <snagula@kent.edu>
2016-07-31 18:40:56 -0500

    First Commit

---
+++ src/org/Harsha/MainApp.java
@@ -0,0 +1,16 @@
+package org.Harsha;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+public static void main(String[] args) {
+	ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+	Student student= (Student) context.getBean("student");
+	
+	student.getAge();
+	student.getName();
+	
+	//student.pointThrowException();
+}
+}
---
+++ src/org/Harsha/Student.java
@@ -0,0 +1,30 @@
+package org.Harsha;
+
+public class Student {
+	private Integer age;
+	private String name;
+	
+	public Integer getAge() {
+		System.out.println("Age : " + age );
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	public String getName() {
+		System.out.println("Name : " + name );
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	/*public void pointThrowException()
+	{
+		System.out.println("Exception Raised:");
+		throw new IllegalArgumentException();
+	}*/
+	
+	
+
+}
